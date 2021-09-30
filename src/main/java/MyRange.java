88c22b07e631fb04cd6a7428f9fefc03639cc43a
NAWAPHONG ONCHARUN (CDGS-ADD) <nawaphong.o@cdg.co.th>
2021-09-30 12:06:07 +0700

    commit test case 01-04

--- src/main/java/MyRange.java
+++ src/main/java/MyRange.java
@@ -1,6 +1,7 @@
 
 public class MyRange {
-
+	private static final char END_NUMBER_POS = 1;
+	private static final int ASCII_TO_NUMBER = 48;
 	private String input;
 
 	public MyRange(String input) {
@@ -10,6 +11,18 @@ public class MyRange {
 	public boolean startWithInclude() {
 		
 		return input.startsWith("[");
+	}
+	
+	public boolean endWithInclude() {
+		return input.endsWith("]");
+	}
+	
+	public int getStart() {
+		return input.charAt(1) - ASCII_TO_NUMBER;
+	}
+
+	public int getEnd() {
+		return input.charAt((input.length() - 1) - END_NUMBER_POS) - ASCII_TO_NUMBER;
 	}
 
 }
--- src/test/java/MyRangeTest.java
+++ src/test/java/MyRangeTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,10 +8,41 @@ public class MyRangeTest {
 	@Test
 	public void case01() {
 		//Arrange
+		//Check start with include
 		String input = "[1,5]";
 		MyRange range = new MyRange(input);
 		boolean result = range.startWithInclude();
 		assertTrue(result);
+	}
+	
+	@Test
+	public void case02() {
+		//Arrange
+		//Check start with include
+		String input = "[1,5]";
+		MyRange range = new MyRange(input);
+		boolean result = range.endWithInclude();
+		assertTrue(result);
+	}
+	
+	@Test
+	public void case03() {
+		//Arrange
+		//Check start with include and return start by 1
+		String input = "[1,5]";
+		MyRange range = new MyRange(input);
+		int result = range.getStart();
+		assertEquals(1, result);
+	}
+	
+	@Test
+	public void case04() {
+		//Arrange
+		//Check start with include and return start by 1
+		String input = "[1,5]";
+		MyRange range = new MyRange(input);
+		int result = range.getEnd();
+		assertEquals(5, result);
 	}
 
 }
