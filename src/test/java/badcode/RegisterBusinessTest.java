831777d9bc3daf2a45dfaab839b5691d408ef0a9
NAWAPHONG ONCHARUN (CDGS-ADD) <nawaphong.o@cdg.co.th>
2021-10-01 10:25:39 +0700

    add more RegisterBusiness Test

--- src/test/java/badcode/RegisterBusinessTest.java
+++ src/test/java/badcode/RegisterBusinessTest.java
@@ -16,9 +16,9 @@ class RegisterBusinessTest {
 			business.register(null, new Speaker());
 		});
 		assertEquals("First name is required.", exception.getMessage());
-		
+
 	}
-	
+
 	@Test
 	@DisplayName("ทดสอบเรียกโดยส่ง First name ว่าง")
 	public void registerTest02() {
@@ -29,9 +29,9 @@ class RegisterBusinessTest {
 			business.register(null, speaker);
 		});
 		assertEquals("First name is required.", exception.getMessage());
-		
+
 	}
-	
+
 	@Test
 	@DisplayName("ทดสอบเรียกโดยส่ง First name")
 	public void registerTest03() {
@@ -42,9 +42,9 @@ class RegisterBusinessTest {
 			business.register(null, speaker);
 		});
 		assertEquals("Last name is required.", exception.getMessage());
-		
+
 	}
-	
+
 	@Test
 	@DisplayName("ทดสอบเรียกโดยส่ง Last name ว่าง")
 	public void registerTest04() {
@@ -56,7 +56,7 @@ class RegisterBusinessTest {
 			business.register(null, speaker);
 		});
 		assertEquals("Last name is required.", exception.getMessage());
-		
+
 	}
 
 	@Test
@@ -70,9 +70,9 @@ class RegisterBusinessTest {
 			business.register(null, speaker);
 		});
 		assertEquals("Email is required.", exception.getMessage());
-		
+
 	}
-	
+
 	@Test
 	@DisplayName("ทดสอบเรียกโดยส่ง Email ว่าง")
 	public void registerTest06() {
@@ -85,9 +85,9 @@ class RegisterBusinessTest {
 			business.register(null, speaker);
 		});
 		assertEquals("Email is required.", exception.getMessage());
-		
+
 	}
-	
+
 	@Test
 	@DisplayName("ทดสอบเรียกโดยส่ง Email")
 	public void registerTest07() {
@@ -100,9 +100,9 @@ class RegisterBusinessTest {
 			business.register(null, speaker);
 		});
 		assertEquals("Speaker doesn't meet our standard rules.", exception.getMessage());
-		
+
 	}
-	
+
 	@Test
 	@DisplayName("ทดสอบเรียกโดยส่ง Email gmail")
 	public void registerTest08() {
@@ -115,6 +115,133 @@ class RegisterBusinessTest {
 			business.register(null, speaker);
 		});
 		assertEquals("Can't save a speaker.", exception.getMessage());
+
+	}
+
+	@Test
+	@DisplayName("ทดสอบเรียกโดยส่ง Email ผิด Syntax")
+	public void registerTest09() {
+		RegisterBusiness business = new RegisterBusiness();
+		Exception exception = assertThrows(DomainEmailInvalidException.class, () -> {
+			Speaker speaker = new Speaker();
+			speaker.setFirstName("nawaphong");
+			speaker.setLastName("oncharun");
+			speaker.setEmail("@nawaphong@gmail.com");
+			business.register(null, speaker);
+		});
+
+	}
+
+	@Test
+	@DisplayName("ทดสอบเรียกโดยสร้าง Repository")
+	public void registerTest10() {
+		RegisterBusiness business = new RegisterBusiness();
+		Exception exception = assertThrows(RuntimeException.class, () -> {
+			Speaker speaker = new Speaker();
+			SpeakerRepository sp = new SpeakerRepository() {
+				@Override
+				public Integer saveSpeaker(Speaker speaker) {
+					throw new RuntimeException("can't save");
+				}
+			};
+			speaker.setFirstName("nawaphong");
+			speaker.setLastName("oncharun");
+			speaker.setEmail("nawaphong@gmail.com");
+			business.register(sp, speaker);
+		});
+		assertEquals("Can't save a speaker.", exception.getMessage());
+	}
+
+	@Test
+	@DisplayName("ทดสอบเรียกโดยสร้าง Repository และส่งค่าสำเร็จ")
+	public void registerTest11() {
+		RegisterBusiness business = new RegisterBusiness();
+		Speaker speaker = new Speaker();
+		SpeakerRepository sp = new SpeakerRepository() {
+			@Override
+			public Integer saveSpeaker(Speaker speaker) {
+				return 100;
+			}
+		};
+		speaker.setFirstName("nawaphong");
+		speaker.setLastName("oncharun");
+		speaker.setEmail("nawaphong@gmail.com");
+		assertEquals(100,business.register(sp, speaker));
+	}
+	
+	@Test
+	@DisplayName("ทดสอบเรียกโดยส่ง Speaker exp=0")
+	public void registerTest12() {
+		RegisterBusiness business = new RegisterBusiness();
+		Speaker speaker = new Speaker();
+		SpeakerRepository sp = new SpeakerRepository() {
+			@Override
+			public Integer saveSpeaker(Speaker speaker) {
+				return speaker.getRegistrationFee();
+			}
+		};
+		speaker.setFirstName("nawaphong");
+		speaker.setLastName("oncharun");
+		speaker.setEmail("nawaphong@gmail.com");
+		speaker.setExp(0);
 		
+		assertEquals(500,business.register(sp, speaker));
+	}
+	
+	@Test
+	@DisplayName("ทดสอบเรียกโดยส่ง Speaker exp=2")
+	public void registerTest13() {
+		RegisterBusiness business = new RegisterBusiness();
+		Speaker speaker = new Speaker();
+		SpeakerRepository sp = new SpeakerRepository() {
+			@Override
+			public Integer saveSpeaker(Speaker speaker) {
+				return speaker.getRegistrationFee();
+			}
+		};
+		speaker.setFirstName("nawaphong");
+		speaker.setLastName("oncharun");
+		speaker.setEmail("nawaphong@gmail.com");
+		speaker.setExp(2);
+		
+		assertEquals(250,business.register(sp, speaker));
+	}
+	
+	@Test
+	@DisplayName("ทดสอบเรียกโดยส่ง Speaker exp=4")
+	public void registerTest14() {
+		RegisterBusiness business = new RegisterBusiness();
+		Speaker speaker = new Speaker();
+		SpeakerRepository sp = new SpeakerRepository() {
+			@Override
+			public Integer saveSpeaker(Speaker speaker) {
+				return speaker.getRegistrationFee();
+			}
+		};
+		speaker.setFirstName("nawaphong");
+		speaker.setLastName("oncharun");
+		speaker.setEmail("nawaphong@gmail.com");
+		speaker.setExp(4);
+		
+		assertEquals(100,business.register(sp, speaker));
+	}
+	
+	@Test
+	@DisplayName("ทดสอบเรียกโดยส่ง Speaker exp=6")
+	public void registerTest15() {
+		RegisterBusiness business = new RegisterBusiness();
+		Speaker speaker = new Speaker();
+		SpeakerRepository sp = new SpeakerRepository() {
+			@Override
+			public Integer saveSpeaker(Speaker speaker) {
+				return speaker.getRegistrationFee();
+			}
+		};
+		speaker.setFirstName("nawaphong");
+		speaker.setLastName("oncharun");
+		speaker.setEmail("nawaphong@gmail.com");
+		speaker.setExp(6);
+		
+		assertEquals(50,business.register(sp, speaker));
 	}
 }
